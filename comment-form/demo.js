a633f607a1bd9be3a8a11feef30e921b828c00b7
Jon Bomgardner <jonlb@comcast.net>
2012-01-12 16:40:24 -0800

    update to comment-form fiddle

--- comment-form/demo.js
+++ comment-form/demo.js
@@ -1,90 +1,92 @@
 window.addEvent('domready', function(){
-    new Jx.Panel({
-        image: 'images/page_white_world.png',
-        hideTitle: true,
-        menu: true,
-        height: 400,
-        id: 'commentPanel',
+    new Jx.Container({
         items: [{
-            'class': 'form',
+            'class': 'panel',
             options: {
-                name: 'commentForm',
-                formClass: 'jxFormInlineblock',
+                parent: 'body',
+                hideTitle: true,
+                id: 'commentPanel',
                 items: [{
-                    //name
-                    'class': 'text',
+                    'class': 'form',
                     options: {
-                        label: 'Name',
-                        name: 'author',
-                        id: 'author',
-                        required: true
-                    }
-                },{
-                    //email
-                    'class': 'text',
-                    options: {
-                        label: 'Email',
-                        name: 'email',
-                        id: 'email',
-                        required: true
-                    }
-                },{
-                    //website
-                    'class': 'text',
-                    options: {
-                        label: 'Website',
-                        name: 'url',
-                        id: 'url',
-                        required: true
-                    }
-                },{
-                    //comment
-                    'class': 'editor',
-                    options: {
-                        name: 'description',
-                        label: 'Description',
-                        id: 'description',
-                        required: true,
-                        value: '',
-                        editorOptions: {
-                            //editorCssFile: 'css/editor.css',
-                            buttons: [
-                                ['bold','italic','underline','strikethrough','separator','alignment',
-                                  'separator','orderedlist','unorderedlist','indent','outdent','separator','undo','redo'],
-                                [{ name: 'customStyles',
-                                   options: {
-                                       styles: ['small','large','hide','quiet','loud','highlight',
-                                            'added','removed','first','last','top','bottom']
-                                    }
-                                },'block', 'separator', 'link','unlink', 'image','separator', 'toggle']
-                            ]
-                        }
-                    }
-                }],
-                plugins: [{
-                    name: 'validator',
-                    options: {
-                        fields: {
-                            'author': {
-                                validators: ['minLength:5', 'maxLength:30']
-                            },
-                            'email': {
-                                //Need to add the empty validators array which will force it to
-                                //also pick up the required option in the field itself.
-                                validators: []
-                            },
-                            'url': {
-                                validators: []
+                        name: 'commentForm',
+                        formClass: 'jxFormBlock',
+                        items: [{
+                            //name
+                            'class': 'text',
+                            options: {
+                                label: 'Name',
+                                name: 'author',
+                                id: 'author',
+                                required: true
                             }
-                        }
-                    }
-                },{
-                    name: 'notifier',
-                    options: {
-                        notifierType: 'inline'
+                        },{
+                            //email
+                            'class': 'text',
+                            options: {
+                                label: 'Email',
+                                name: 'email',
+                                id: 'email',
+                                required: true
+                            }
+                        },{
+                            //website
+                            'class': 'text',
+                            options: {
+                                label: 'Website',
+                                name: 'url',
+                                id: 'url',
+                                required: true
+                            }
+                        },{
+                            //comment
+                            'class': 'editor',
+                            options: {
+                                name: 'description',
+                                label: 'Description',
+                                id: 'description',
+                                required: true,
+                                value: '',
+                                editorOptions: {
+                                    buttons: [
+                                        ['bold','italic','underline','strikethrough','separator','alignment',
+                                          'separator','orderedlist','unorderedlist','indent','outdent','separator','undo','redo'],
+                                        [{ name: 'customStyles',
+                                           options: {
+                                               styles: ['small','large','hide','quiet','loud','highlight',
+                                                    'added','removed','first','last','top','bottom']
+                                            }
+                                        },'block', 'separator', 'link','unlink', 'image','separator', 'toggle']
+                                    ]
+                                }
+                            }
+                        }],
+                        plugins: [{
+                            name: 'validator',
+                            options: {
+                                fields: {
+                                    'author': {
+                                        validators: ['minLength:5', 'maxLength:30']
+                                    },
+                                    'email': {
+                                        //Need to add the empty validators array which will force it to
+                                        //also pick up the required option in the field itself.
+                                        validators: []
+                                    },
+                                    'url': {
+                                        validators: []
+                                    }
+                                }
+                            }
+                        },{
+                            name: 'notifier',
+                            options: {
+                                notifierType: 'inline'
+                            }
+                        }]
                     }
                 }]
             }
         }]
-    });  
+    });
 });
